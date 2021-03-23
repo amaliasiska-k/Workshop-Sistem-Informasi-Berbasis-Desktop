d727d4b19594253ac7b0714076bc3ebadfaac378
amaliasiska39 <amaliasiska39@gmail.com>
2021-03-23 13:09:17 +0700

    Create Tampilan_Nama.java

---
+++ Tugas Minggu-3/Tampilan_Nama.java
@@ -0,0 +1,14 @@
+package tugasminggu3;
+/**
+ * NAMA  : AMALIA SISKA KURNIAWATI
+ * NIM   : E41201005
+ */
+public class Tampilan_Nama {
+     public static void main(String[] args) {
+        String[] nama = {"Amalia", "Siska", "Kurniawati"};
+            for (String nama1 : nama) {
+            System.out.print (nama1 + " ");
+         }
+    }
+}
+ 
