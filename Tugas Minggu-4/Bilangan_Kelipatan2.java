0a4382e74c4d46cd3a139fc2e3a1ea7417b4fe31
amaliasiska39 <amaliasiska39@gmail.com>
2021-04-02 09:39:07 +0700

    Create Bilangan_Kelipatan2.java

---
+++ Tugas Minggu-4/Bilangan_Kelipatan2.java
@@ -0,0 +1,22 @@
+package tugasminggu4;
+
+/**
+ * NAMA  : AMALIA SISKA KURNIAWATI
+ * NIM   : E41201005
+ * GOL   : A - BONDOWOSO
+ */
+
+public class Bilangan_Kelipatan2 {
+    
+    public static void main(String[] args) {
+        int nilai = 1;
+        System.out.println("         Do__While");
+        System.out.println("Bilangan kelipatan 2 ( 0 - 100 )");
+        System.out.println("================================");
+        do {
+            System.out.print("  " + nilai);
+            nilai = nilai * 2;
+        } while (nilai < 100);
+        System.out.println("");
+    }
+} 
